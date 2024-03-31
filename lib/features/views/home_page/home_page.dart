faa30d91dbe3a4d3151d0a476b4b5127d4bbaa0f
raju-islam <rajuslam39@gmail.com>
2024-03-31 16:54:11 +0600

    appbar make now

--- lib/features/views/home_page/home_page.dart
+++ lib/features/views/home_page/home_page.dart
@@ -1,73 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:teacher_app/common/widget/app_bar/app_bar.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              Container(
-                width: double.infinity,
-                height: 200,
-                decoration: const BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(50),
-                    bottomRight: Radius.circular(50)
-                  )
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            IconButton(onPressed: (){}, icon:
-                            const Icon(Icons.menu,color: Colors.white,size: 30,)),
-                            const Text("DASHBOARD",style: TextStyle(color: Colors.white,fontSize: 30),),
-                          ],
-                        ),
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
 
-                        const Icon(Icons.notifications_active,color: Colors.white,size: 30,)
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(left: 20,right: 20),
-                      child: Card(
-                        shadowColor: Colors.black,
-                        color: Colors.white,
-                        child: SizedBox(
-                          height: 200,
-                          width: double.infinity,
-                          child: Center(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Icon(Icons.person,size: 40,),
-                                Text("Raju Islam",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-                                Text("STD -12(B)")
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-        ],
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            const RAppBar(height: 200),
+             Padding(
+         padding: const EdgeInsets.only(
+           top: 100
+         ),
+         child: Column(
+           children: [
+             Row(
+               mainAxisAlignment: MainAxisAlignment.center,
+               children: [
+                 Card(
+                   child: SizedBox(
+                     height: height*0.3,
+                     width: width*0.9,
+                     child: const Column(
+                       children: [
+                         Icon(Icons.person,size: 50,)
+                       ],
+                     ),
+                   ),
+                 ),
+               ],
+             ),
+           ],
+         ),
+             )
+          ],
+        ),
       ),
     );
   }
